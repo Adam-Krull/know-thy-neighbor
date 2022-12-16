139380780d0b931f1b5d383182e81a80484e9846
Adam Krull <aqkrull@gmail.com>
2022-12-16 16:52:26 -0600

    establish connection to DP03 data

--- acquire.py
+++ acquire.py
@@ -82,3 +82,102 @@ def prep_s1901(df):
     df = df[['label', 'value']]
     
     return df
+
+
+def get_2020_profile_data(year, zip_code, group='DP03'):
+    
+    url = f'https://api.census.gov/data/{year}/acs/acs5/profile?get=group({group})&for=zip%20code%20tabulation%20area:{zip_code}&key={census_api_key}'
+    
+    response = get(url)
+    
+    content = response.json()
+    
+    df = pd.DataFrame(content).T
+    
+    df.columns=['variable', year]
+    
+    regex = '^DP03_\d*E$'
+
+    df = df[df['variable'].str.match(regex)]
+
+    df = df[df[year] != '-888888888']
+
+    df.reset_index(drop=True, inplace=True)
+    
+    return df
+
+
+def get_pre_2020_profile_data(year, zip_code, group='DP03'):
+    
+    url = f'https://api.census.gov/data/{year}/acs/acs5/profile?get=group({group})&for=zip%20code%20tabulation%20area:{zip_code}&in=state:48&key={census_api_key}'
+    
+    response = get(url)
+    
+    content = response.json()
+    
+    df = pd.DataFrame(content).T
+    
+    df.columns=['variable', year]
+    
+    regex = '^DP03_\d*E$'
+
+    df = df[df['variable'].str.match(regex)]
+
+    df = df[df[year] != '-888888888']
+
+    df.reset_index(drop=True, inplace=True)
+    
+    return df
+
+
+def assemble_group_data(zip_code, years=list(map(str, list(range(2012, 2020))))):
+    
+    final_df = pd.DataFrame()
+    
+    for year in years:
+        
+        df = get_pre_2020_profile_data(year, zip_code)
+        
+        if len(final_df) == 0:
+            
+            final_df = df.copy()
+            
+        else:
+            
+            final_df = final_df.merge(df, how='left', on='variable')
+        
+    new_df = get_2020_profile_data('2020', zip_code)
+    
+    final_df = final_df.merge(new_df, how='left', on='variable')
+            
+    return final_df  
+
+
+def process_dp03_data(df):
+    
+    variable_list = df['variable'].tolist()
+
+    label_list = []
+
+    base_url = 'https://api.census.gov/data/2012/acs/acs5/profile/variables/'
+
+    for var in variable_list:
+    
+        url = base_url + var + '.json'
+    
+        response = get(url)
+    
+        content = response.json()
+    
+        label_list.append(content['label'].split('!!')[-1])
+    
+    df['variable'] = pd.Series(label_list)
+    
+    return df
+
+
+def acquire_data(zip_code):
+    
+    df = assemble_group_data(zip_code)
+    
+    return process_dp03_data(df)
